fa71702abb9153ba5c1241cb5f3d6f40b8c6aac9
Aniruddha Bera <aniruddhabera23@gmail.com>
2025-08-12 21:54:37 +0530

    Add files via upload

---
+++ Assignment_1_Q2.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    float arr[100];
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    printf("Enter the elements:\n");
+    for (i = 0; i < n; ++i) {
+        scanf("%f", &arr[i]);
+    }
+
+    
+    float largest = arr[0];
+ for (i = 1; i < n; ++i) {
+        if (arr[i] > largest) {
+            largest = arr[i];
+        }
+    }
+
+    printf("The largest element is: %.2f\n", largest);
+
+    return 0;
+}
